1a9abb161228eaa5e94e395507fde04d3c498c44
DinaTaklit <ez_taklit@esi.dz>
2020-09-10 14:13:40 +0100

    1. IdentityFunctions: Add simple functions

---
+++ 1.IdentityFunction.js
@@ -0,0 +1,40 @@
+/**
+ * Write an identity function that makes an argument and returns that argument.
+ *  identity(3) // 3
+ * **/
+
+
+ function identity(number){
+     return number;
+ }
+
+console.log(identity(3));
+
+var identity2 = function identity2(x){
+    return x;
+};
+
+console.log(identity2(4));
+
+/**
+ *  Write 03 binary functions, add, sub, and mul, that thake * two numbers and return their sum, difference, and product.
+ *  add(3, 4)  // 7 
+ *  sub(3, 4) // -1
+ *  mul(3, 4) // 12
+ * **/
+
+ function add(x,y){
+     return x + y;
+ }
+
+ function sub(x,y){
+     return x - y;
+ }
+
+ function mul(x,y){
+     return x * y;
+ }
+
+ console.log(`add(3, 4)= ${add(3, 4)}`);
+ console.log(`sub(3, 4)= ${sub(3, 4)}`);
+ console.log(`mul(3, 4)= ${mul(3, 4)}`);
